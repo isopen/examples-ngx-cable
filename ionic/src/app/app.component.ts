389d287fe3511b3cb7574379228b0c149ea57db7
Maxim Tyulenev <mr_makss@mail.ru>
2018-02-11 20:47:27 +0700

    features for ionic

--- ionic/src/app/app.component.ts
+++ ionic/src/app/app.component.ts
@@ -5,18 +5,49 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { NgXCable, Broadcaster } from 'ngx-cable';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    ngcable: NgXCable,
+    broadcaster: Broadcaster
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      const back_ws_host = 'ws://localhost:28080';
+
+      broadcaster.on(back_ws_host)
+        .subscribe(
+          response => {
+            console.log(response);
+          }
+        );
+
+      ngcable.connect(back_ws_host);
+
+      const params = {
+        channel: back_ws_host,
+        room: 1
+      };
+
+      ngcable.subscribe(params);
+
+      params['room'] = 2;
+
+      ngcable.subscribe(params);
+
     });
   }
 }
--- ionic/src/app/app.module.ts
+++ ionic/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { NgXCableModule } from 'ngx-cable';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-import { NgXCableModule } from 'ngx-cable';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -22,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    NgXCableModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
